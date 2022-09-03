398f6783647c50822d60adfcc2f6dcd843ac79b8
Dominik Kormann <dominikormann@outlook.com>
2022-09-03 17:10:50 +0200

    monte carlo

--- monte.py
+++ monte.py
@@ -7,14 +7,29 @@ class Monte(Player):
     """Monte carlo player"""
 
     def __init__(self):
-        
-        #employ dynamic programming so we remember searched states
-        self.memory = {}
+        pass
     
     def choose_move(self, game_board:FourInaRow):
         
-        options = game_board.possible_moves
+        root = game_board
+
+        for i in range (1000):
+            root.expand()
+        return root.best_option
+
+        
         
 
 class SearchNode:
-    pass
+
+    def __init__(self,board:FourInaRow):
+        self.board = board
+        self.ucb = 0
+        self.options = self.board.possible_moves
+        self.best_option = self.options[0]
+        
+    def expand(self):
+        #TODO: implement monte carlo 
+        pass
+
+
